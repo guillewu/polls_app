44d5a950e4d96e9ea854823f953d7177954d3d71
App Academy Student <student@appacademy.io>
2013-10-03 13:00:00 -0700

    correct associations

--- app/models/answer_choice.rb
+++ app/models/answer_choice.rb
@@ -1,16 +1,16 @@
 class AnswerChoice < ActiveRecord::Base
   attr_accessible :text, :q_id
 
-  validates :text, :presence => true,
+  validates :text, :presence => true
   validates :q_id, :presence => true
 
   belongs_to :question,
-            :class => "Question",
-            :foreignkey => :q_id,
+            :class_name => "Question",
+            :foreign_key => :q_id,
             :primary_key => :id
 
-  has_many :renponses,
-            :class => "Response",
+  has_many :responses,
+            :class_name => "Response",
             :foreign_key => :response_id,
             :primary_key => :id
 
--- app/models/response.rb
+++ app/models/response.rb
@@ -4,13 +4,25 @@ class Response < ActiveRecord::Base
   validates :answer_id, :presence => true
   validates :user_id, :presence => true
 
+  validate :respondent_has_not_already_answered_question
+
   belongs_to :answer_choice,
-             :class => "AnswerChoice",
+             :class_name => "AnswerChoice",
             :foreign_key => :answer_id,
             :primary_key => :id
 
   belongs_to :respondent,
-            :class => "User",
+            :class_name => "User",
             :foreign_key => :user_id,
             :primary_key => :id
+
+
+  def respondent_has_not_already_answered_question
+
+  end
+
+
+
+  #answer_choice.question #returns question of what answer
+
 end
